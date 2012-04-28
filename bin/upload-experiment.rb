726b62986a0415359dee0b7f813c0d094178e284
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-04-28 16:53:21 +0200

    Buckets fetched
    The buckets are fetched.

--- bin/upload-experiment.rb
+++ bin/upload-experiment.rb
@@ -5,4 +5,17 @@ require "bundler/setup"
 
 require "aws/s3"
 
-puts "Hello World"
+include AWS::S3
+
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV['AMAZON_ACCESS_KEY_ID'],
+  :secret_access_key => ENV['AMAZON_SECRET_ACCESS_KEY'],
+  :use_ssl => true,
+)
+
+
+Service.buckets.each do |bucket|
+  if (bucket.name == '37buckets') then
+    puts bucket.name
+  end
+end
